34d24a55077f3cfda11976317031f68660a58975
Elvin Shoyfer <eshoyfer@gmail.com>
2015-08-13 06:57:21 -0400

    Almost proper copying

--- collector.py
+++ collector.py
@@ -4,20 +4,41 @@ import fileinput
 import glob
 import shutil
 
-## Reading Initial Arguments ## 
+## Begin Reading Initial Arguments ## 
 
-if len(sys.argv) > 1:
-	# AppData folder supplied
-	input_dir = sys.argv[1]
-	specified_path = os.path.join(input_dir, 'Local', 'qBittorrent', 'BT_backup')
+# Intended usage: python collector.py <source_dir> [<dest_dir>]
+
+if len(sys.argv) > 2:
+	# Source and destination folder supplied
+	source_dir = os.path.abspath(sys.argv[1])
+	dest_dir = os.path.abspath(sys.argv[2])
+elif len(sys.argv) > 1:
+	# Source folder supplied
+	source_dir = os.path.abspath(sys.argv[1])
+	dest_dir = os.path.abspath('./output') # Defaults to output folder in current directory 
 else:
-	# Default 
-	# No AppData folder supplied, locate default
-	default_path = os.path.join('C:', os.sep, 'Users', os.getenv('username'), 'AppData', 'Local', 'qBittorrent', 'BT_backup')
-	specified_path = default_path
+	# Neither source nor destination folder supplied, exit
+	sys.exit("Invalid arguments! Intended usage: python collector.py <source_dir> <dest_dir>(optional)")
+	
+print "Source directory: ", source_dir
+print "Destination directory: ", dest_dir
 
-if not os.path.exists(specified_path):
-	sys.exit("Invalid path!\nThe folder is typically located at C:\Users\<user>\AppData.\nSupply no arguments to attempt finding the default folder.")
+if not os.path.exists(source_dir):
+    os.makedirs(source_dir)
 
+if not os.path.exists(dest_dir):
+	os.makedirs(dest_dir)
+
+if not (os.path.exists(source_dir) and os.path.exists(dest_dir)):
+	sys.exit("Invalid directories specified!")
 
 ## End Reading Initial Arguments ## 
+
+## Begin Copying Torrent Files ##
+
+files = glob.iglob(os.path.join(source_dir, "*.torrent"))
+for file in files:
+    if os.path.isfile(file):
+        shutil.copy2(file, dest_dir)
+
+## End Copying Torrent Files ##
